6f144e2f73a76e55a924219e7bf1f960d1b45ba1
5rshv <serishev.alesha@gmail.com>
2025-07-15 21:04:10 +0300

    Создал подключение к 2 БД, Написан репозиторий и сервис, + заглушки

---
+++ src/main/java/com/example/credit_product/dto/DynamicRuleDTO.java
@@ -0,0 +1,36 @@
+package com.example.credit_product.dto;
+
+public class DynamicRuleDTO {
+    public String getProductName() {
+        return null;
+    }
+
+    public String getProductId() {
+        return null;
+    }
+
+    public String getProductText() {
+        return null;
+    }
+
+    public Object getRule() {
+        return null;
+    }
+
+    public void setId(String string) {
+        
+    }
+
+    public void setProductName(String productName) {
+        
+    }
+
+    public void setProductId(String string) {
+    }
+
+    public void setProductText(String productText) {
+    }
+
+    public void setRule(Object o) {
+    }
+}
---
+++ src/main/java/com/example/credit_product/repository/DynamicRuleRepository.java
@@ -0,0 +1,18 @@
+package com.example.credit_product.repository;
+
+import com.example.credit_product.model.DynamicRule;
+
+import java.util.UUID;
+
+public class DynamicRuleRepository {
+    public void deleteById(UUID id) {
+    }
+
+    public DynamicRule save(DynamicRule rule) {
+        return null;
+    }
+
+    public Iterable<Object> findAll() {
+        return null;
+    }
+}
